39242f9985d3798a150b04fd1723aecbbbe17f05
gaohuihui <582286857@qq.com>
2021-10-12 14:21:49 +0800

    1009

--- 20211009.py
+++ 20211009.py
@@ -1,4 +1,5 @@
 '''
+https://leetcode-cn.com/problems/find-the-duplicate-number/
 description:找出数组中重复的数字。
 
 在一个长度为 n 的数组 nums 里的所有数字都在 0 ～ n-1 的范围内。数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。请找出数组中任意一个重复的数字。
@@ -17,7 +18,29 @@ description:找出数组中重复的数字。
 class Solution():
     def findRepeatNumber(self, nums):
 
-        pass
+        lenth=len(nums)
+
+        while (lenth>0):
+            number=nums.pop()
+            nums=nums
+            if (number in nums)==True:
+                return number
+                break
+            lenth=len(nums)
+
+
+
+        return None
+
+
+
+
+if __name__=="__main__":
+    nums=[2, 1, 0, 5, 3,2]
+    print(hash(nums))
+
+
+
 
 
 
